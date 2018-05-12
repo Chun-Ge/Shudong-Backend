10ec288e6382f502dba6afbe9ea9d7036fe943c6
alexandrali <fjcyslyz@gmail.com>
2018-05-12 19:26:46 +0800

    Modify struct User

--- vendor/entity/user.go
+++ vendor/entity/user.go
@@ -5,8 +5,9 @@ import "time"
 // User represents users of Shudong.
 type User struct {
 	ID         int64
-	Username   string    `xorm:"varchar(50) notnull unique"`
+	Email      string    `xorm:"notnull unique"`
 	Password   string    `xorm:"varchar(32) notnull"`
+	Username   string    `xorm:"varchar(50)"`
 	CreateTime time.Time `xorm:"created DATETIME"`
 	UpdateTime time.Time `xorm:"updated DATETIME"`
 }
