b9d651a008f7e9a10ac968a785a64d8276426c6c
MarshallW906 <MaticWang@hotmail.com>
2018-05-14 16:42:08 +0800

    fix tiny error : err.CheckErr()

--- vendor/model/user.go
+++ vendor/model/user.go
@@ -3,11 +3,12 @@ package model
 import (
 	"database"
 	"entity"
+	"err"
 )
 
 // GetUser .
-func GetUser(username string, password string) (user *entity.User, err error) {
-	_, err = database.Orm.Where("username=? and password=?", username, password).Get(&user)
-	checkErr(err)
+func GetUser(username string, password string) (user *entity.User, er error) {
+	_, er = database.Orm.Where("username=? and password=?", username, password).Get(&user)
+	err.CheckErr(er)
 	return
 }
