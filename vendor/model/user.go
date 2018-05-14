0b1be85a99cc702c45d991096d4beb16868c1fde
alexandrali <fjcyslyz@gmail.com>
2018-05-15 00:46:39 +0800

    Fix small error in model/user.go

--- vendor/model/user.go
+++ vendor/model/user.go
@@ -3,11 +3,12 @@ package model
 import (
 	"database"
 	"entity"
+	e "err"
 )
 
 // GetUser .
 func GetUser(username string, password string) (user *entity.User, err error) {
 	_, err = database.Orm.Where("username=? and password=?", username, password).Get(&user)
-	checkErr(err)
+	e.CheckErr(err)
 	return
 }
