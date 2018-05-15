6aeb8691d54d5d33546c9e18f5a71318279e45a8
MarshallW906 <MaticWang@hotmail.com>
2018-05-15 23:48:32 +0800

    skeleton of UpvotePost()

--- vendor/service/user_upvote_post.go
+++ vendor/service/user_upvote_post.go
@@ -9,12 +9,38 @@ import (
 // UpvotePost ..
 func UpvotePost(ctx iris.Context) {
 	// read cookie from ctx, (check login info?), then model.CheckPostIfUpvoted()
+
+	var (
+		userid     int64 = 1
+		postid     int64 = 1
+		affected   int64 // = 0
+		isLoggedIn = true
+		// er         error
+	)
+
+	if !isLoggedIn {
+		// Unauthorized
+		return
+	}
+	upvoted, _ := model.CheckPostIfUpvoted(userid, postid)
+	if upvoted {
+		// upvotePost(userid, postid)
+	} else {
+		// upvotePostCancel(userid, postid)
+	}
+	if affected <= 0 {
+		// Internal Server Error
+	} else {
+		// OK
+	}
 }
 
 func upvotePost(userid, postid int64) (affected int64) {
-	return model.UpvotePostByUser(userid, postid)
+	affected, _ = model.UpvotePostByUser(userid, postid)
+	return
 }
 
 func upvotePostCancel(userid, postid int64) (affected int64) {
-	return model.CancelUpvotePostByUser(userid, postid)
+	affected, _ = model.CancelUpvotePostByUser(userid, postid)
+	return
 }
