896855c2c5c5cf54c92b06bf88c1f989b237b337
alexandrali <fjcyslyz@gmail.com>
2018-05-17 02:40:20 +0800

    Fix tiny spelling errors

--- vendor/service/post.go
+++ vendor/service/post.go
@@ -3,22 +3,24 @@ package service
 import (
 	"args"
 	"model"
+	"response"
+
 	"github.com/kataras/iris"
 
-    jwt "github.com/dgrijalva/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
 	jwtmiddleware "github.com/iris-contrib/middleware/jwt"
 )
 
 type postInfo struct {
-    UserID int64
-    CategoryID int64 `form:"category"`
-    Title string `form:"title"`
-    Content string `form:"content"`
+	UserID     int64
+	CategoryID int64  `form:"category"`
+	Title      string `form:"title"`
+	Content    string `form:"content"`
 }
 
-// post a Post
+// CreatePost Post a post.
 func CreatePost(ctx iris.Context) {
-    myJwtMiddleware := jwtmiddleware.New(jwtmiddleware.Config{
+	myJwtMiddleware := jwtmiddleware.New(jwtmiddleware.Config{
 		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
 			return []byte(args.SecretKey), nil
 		},
@@ -27,43 +29,41 @@ func CreatePost(ctx iris.Context) {
 		Expiration:    true,
 	})
 
-    userToken := myJwtMiddleware.Get(ctx)
-    if claims, ok := userToken.Claims.(jwt.MapClaims); ok && userToken.Valid {
-        // claimTestedValue = claims["foo"].(string)
-        userId := claims["id"].(int64)
+	userToken := myJwtMiddleware.Get(ctx)
+	if claims, ok := userToken.Claims.(jwt.MapClaims); ok && userToken.Valid {
+		// claimTestedValue = claims["foo"].(string)
+		userID := claims["id"].(int64)
 
-        info := postInfo{UserID : userId}
-        ctx.ReadForm(&info)
+		info := postInfo{UserID: userID}
+		ctx.ReadForm(&info)
 
-        post, err := model.NewPostWithRandomName(info.UserID, info.CategoryID, info.Title, info.Content)
-        if err != nil {
-            ctx.StatusCode(iris.StatusInternalServerError) // code: 500
-            ctx.JSON(iris.Map{
-                "msg" : "InternalServerError",
-                //"data" : ""
-            })
-            return
-        }
+		post, err := model.NewPostWithRandomName(info.UserID, info.CategoryID, info.Title, info.Content)
+		if err != nil {
+			ctx.StatusCode(iris.StatusInternalServerError) // code: 500
+			ctx.JSON(iris.Map{
+				"msg": "InternalServerError",
+				//"data" : ""
+			})
+			return
+		}
 
-        ctx.StatusCode(iris.StatusOK) // code: ok
-        ctx.JSON(iris.Map{
-            "msg" : "OK",
-            "data" : iris.Map{
-                "post" : iris.Map{
-                    "postId": post.ID,
-                    "authorId": post.UserID,
-                    "title": post.Title,
-                    "content": post.Content,
-                    "likeCount": post.Like,
-                    "commentCount": 0 , //
-                },
-            },
-        })
-    } else {
-        ctx.StatusCode(iris.StatusInternalServerError) // code: 500
-        ctx.JSON(iris.Map{
-            "msg" : "InternalServerError",
-            //"data" : ""
-        })
-    }
+		response.OK(ctx, iris.Map{
+			"msg": "OK",
+			"data": iris.Map{
+				"post": iris.Map{
+					"postId":       post.ID,
+					"authorId":     post.UserID,
+					"title":        post.Title,
+					"content":      post.Content,
+					"likeCount":    post.Like,
+					"commentCount": 0, //
+				},
+			},
+		})
+	} else {
+		response.InternalServerError(ctx, iris.Map{
+			"msg": "InternalServerError",
+			//"data" : ""
+		})
+	}
 }
