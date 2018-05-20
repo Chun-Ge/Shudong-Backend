cbbe0cb89bacdb2c86d6dfebf7fa4a01ba61d04b
MarshallW906 <MaticWang@hotmail.com>
2018-05-20 13:14:54 +0800

    add CheckErrWithCallback()

--- vendor/err/err.go
+++ vendor/err/err.go
@@ -1,8 +1,27 @@
 package err
 
-// CheckErr .
+// Callback .
+type Callback func()
+
+// CheckErr checks if an error exists and calls panic() if so.
+//
+// Deprecated: Use CheckErrWithPanic() instead.
 func CheckErr(e error) {
 	if e != nil {
 		panic(e)
+	}
+}
+
+// CheckErrWithPanic is simply an alias of CheckErr(), to avoid unnecessary changes.
+// Use CheckErrWithPanic if panic() is needed.
+func CheckErrWithPanic(e error) {
+	CheckErr(e)
+}
+
+// CheckErrWithCallback checks if an error exists
+// and calls callback() if so.
+func CheckErrWithCallback(e error, callback Callback) {
+	if e != nil {
+		callback()
 	}
 }
