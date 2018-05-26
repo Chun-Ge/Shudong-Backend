0f0170e769514a4d7ad87b4cb6f8d6c3aed2f1ad
alexandrali <fjcyslyz@gmail.com>
2018-05-27 04:05:21 +0800

    Add entity for report comment

---
+++ vendor/entity/report_comment.go
@@ -0,0 +1,15 @@
+package entity
+
+import "time"
+
+// ReportComment .
+type ReportComment struct {
+	ID         int64
+	UserID     int64     `xorm:"notnull"`
+	CommentID  int64     `xorm:"notnull`
+	Reason     string    `xorm:"TEXT`
+	CreateDate time.Time `xorm:"created DATETIME"`
+}
+
+// ReportComments .
+type ReportComments []*ReportComment
