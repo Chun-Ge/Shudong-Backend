537c675e0414d46a5ecf9e5d601067c9583012d8
MarshallW906 <MaticWang@hotmail.com>
2018-05-28 23:41:21 +0800

    use CamelCase

--- vendor/args/args.go
+++ vendor/args/args.go
@@ -10,8 +10,10 @@ const (
 	SecretKey = "My Secret"
 	// this const arg should in args/args.go
 
-	// auth code size
-	Auth_Code_Size = 6
-	// the lefe-time of auth code, default: 30minutes
-	Auth_Code_Life_Time = 30
+	// AuthCodeSize ..
+	AuthCodeSize = 6
+
+	// AuthCodeLifeTime is the life time of auth code (minutes)
+	// default to 30.
+	AuthCodeLifeTime = 30
 )
