2b29c3441b2c3266c573ccdb1d03432c6865dc48
MarshallW906 <MaticWang@hotmail.com>
2018-06-02 15:59:27 +0800

    some args: const -> var

--- vendor/args/args.go
+++ vendor/args/args.go
@@ -3,9 +3,7 @@ package args
 var (
 	// Port ...
 	Port = ":8080"
-)
 
-const (
 	// DEBUG ...
 	DEBUG = true
 
@@ -21,7 +19,9 @@ const (
 
 	// DeleteLogFileOnExit ...
 	DeleteLogFileOnExit = false
+)
 
+const (
 	// TimeFormat must be specified with this time exactly.
 	// "2006-01-02 15:04:05"
 	// see https://www.jianshu.com/p/c7f7fbb16932
