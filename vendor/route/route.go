1311264d59fc9a43623886b0590c145823e856ec
Boyce-Lee <1772729359@qq.com>
2018-06-03 15:57:23 +0800

    fix route.go

--- vendor/route/route.go
+++ vendor/route/route.go
@@ -37,6 +37,13 @@ func registerUserRoutes(app *iris.Application) {
 	userRoutes.Use(middlewares.CheckLoginStatus)
 
 	userRoutes.Patch("/password", service.ChangePassword)
+
+	// User Operation url
+	// Retrieve user info
+	// userRoutes.Get("{userId:int min(1)}", handler)
+	// userRoutes.PUT("{userId:int min(1)}", handler)
+	// userRoutes.PATCH("{userId:int min(1)}", handler)
+	// userRoutes.Delete("{userId:int min(1)}", handler)
 }
 
 // registerPostRoutes .
@@ -45,32 +52,46 @@ func registerPostRoutes(app *iris.Application) {
 	postRoutes := app.Party("/posts")
 	postRoutes.Use(middlewares.CheckLoginStatus)
 
-	// add any subpath below
-	// postRoutes.Get("/", service.GetPosts)
-	// postRoutes.Get("/{postid:int min(1)}", service.GetPostByID)
+	// subpath of "/posts"
+	// Post Collection and Creation
+	// postRoutes.Get("/", handler)
 	postRoutes.Post("/", service.CreatePost)
-	postRoutes.Delete("/", service.DeletePost)
 
-	postRoutes.Get("/{postid:int min(1)}/like", service.UpvotePost)
+	// Get and Delete Post
+	// postRoutes.Get("/{postId:int min(1)", handler)
+	postRoutes.Delete("/{postId:int min(1)", service.DeletePost)
 
-	postRoutes.Post("/{postid:int min(1)}/report", service.CreateReportPost)
+	// share a post
+	// postRoutes.Get("/{postId:int min(1)/share", handler)
 
-	postRoutes.Get("/{postid:int min(1)}/star", service.StarPost)
+	// liek/un-like a post
+	postRoutes.Get("/{postId:int min(1)}/like", service.UpvotePost)
+
+	// report a post
+	postRoutes.Post("/{postId:int min(1)}/report", service.CreateReportPost)
+
+	// star a post
+	postRoutes.Get("/{postId:int min(1)}/star", service.StarPost)
 }
 
 // registerCommentRoutes .
-// Group url of "/post/{postid}/comments"
+// Group url of "/post/{postId}/comments"
 func registerCommentRoutes(app *iris.Application) {
-	// redundant API "/comments" for "/posts/{postid:int min(1)}/comments"
-	commentRoutes := app.Party("/posts/{postid:int min(1)}/comments")
+	// redundant API "/comments" for "/posts/{postId:int min(1)}/comments"
+	commentRoutes := app.Party("/posts/{postId:int min(1)}/comments")
 	commentRoutes.Use(middlewares.CheckLoginStatus)
 
-	// add any subpath below
-	// app.Get("/", service.GetComments)
+	// add any subpath of "/posts/{postId:int min(1)}/comments"
+	// Comment Collection and Creation
+	// commentRoutes.Get("/", handler)
 	commentRoutes.Post("/", service.CreateComment)
-	commentRoutes.Delete("/", service.DeleteComment)
 
-	commentRoutes.Get("/{commentid:int min(1)}/like", service.UpvoteComment)
+	// delete comment
+	commentRoutes.Delete("/{commentId:int min(1)", service.DeleteComment)
 
-	commentRoutes.Post("/{commentid:int min(1)}/report", service.CreateReportComment)
+	// like/un-like a comment
+	commentRoutes.Get("/{commentId:int min(1)}/like", service.UpvoteComment)
+
+	// resport a comment
+	commentRoutes.Post("/{commentId:int min(1)}/report", service.CreateReportComment)
 }
